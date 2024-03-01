37db6e73bc67ece9aeab4420935cbef7e89cbb17
Carabalone <guilherme.caraba@gmail.com>
2024-03-01 20:15:46 -0300

    feat: Client now loads node and other clients configs. Also added client main command loop

--- HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/Client.java
+++ HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/Client.java
@@ -2,18 +2,67 @@ package pt.ulisboa.tecnico.hdsledger.client;
 
 import pt.ulisboa.tecnico.hdsledger.utilities.CustomLogger;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
+import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
+
 import java.util.List;
+import java.util.Scanner;
 
 public class Client {
     private static final CustomLogger LOGGER = new CustomLogger(Client.class.getName());
+
+    private final String nodePath = "../Service/src/main/java/resources/regular_config.json";
+    private final String clientPath = "src/main/java/resources/regular_config.json";
 
     private ProcessConfig config;
     private ProcessConfig[] nodes;
     private ProcessConfig[] clients;
 
+    public Client() {
+        nodes = new ProcessConfigBuilder().fromFile(nodePath);
+        clients = new ProcessConfigBuilder().fromFile(clientPath);
+    }
+
+    private static void help() {
+        System.out.println("List of commands: ");
+        System.out.println("append <string_to_append>");
+    }
+
     public final static void main(String[] args) {
         String clientId = args[0];
 
-        System.out.println("I'm alive: " + clientId);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("I'm alive! " + clientId);
+
+        System.out.println("You can start pressing commands: ");
+        Client.help();
+
+        while (true) {
+            System.out.print("$ ");
+
+            String line = scanner.nextLine().trim();
+            String[] terms = line.split("\\s+");
+
+            if (terms.length < 1)
+                System.out.println("Input Something.");
+
+            String command = terms[0];
+
+            switch (command) {
+                case "help" -> {
+                    help();
+                    break;
+                }
+                case "append" -> {
+                    if (terms.length < 2 )
+                        System.out.println("bad input, usage: append <string_to_append>");
+                    System.out.println("appending " + terms[1]);
+                }
+                default -> {
+                    System.out.println("unrecognized command");
+                }
+            }
+
+        }
     }
 }
