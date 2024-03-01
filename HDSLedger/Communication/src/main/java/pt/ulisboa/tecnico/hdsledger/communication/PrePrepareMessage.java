27d71fa260deea80255a97733e22325b5911f1b9
JoãoFurtado <joao.melo.furtado@tecnico.ulisboa.pt>
2024-03-01 19:34:33 +0000

    feat: pre-prepare and prepare signatures

--- HDSLedger/Communication/src/main/java/pt/ulisboa/tecnico/hdsledger/communication/PrePrepareMessage.java
+++ HDSLedger/Communication/src/main/java/pt/ulisboa/tecnico/hdsledger/communication/PrePrepareMessage.java
@@ -3,19 +3,33 @@ package pt.ulisboa.tecnico.hdsledger.communication;
 import com.google.gson.Gson;
 
 public class PrePrepareMessage {
-    
-    // Value
+
     private String value;
 
-    public PrePrepareMessage(String value) {
+    private String leaderSignature;
+
+    public PrePrepareMessage(String value, String leaderSignature) {
         this.value = value;
+        this.leaderSignature = leaderSignature;
     }
 
     public String getValue() {
         return value;
     }
 
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getLeaderSignature() {
+        return leaderSignature;
+    }
+
+    public void setLeaderSignature(String leaderSignature) {
+        this.leaderSignature = leaderSignature;
+    }
+
     public String toJson() {
         return new Gson().toJson(this);
     }
-}   
+}
